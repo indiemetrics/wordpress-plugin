ac8ee154ffd7e21711897e25bf18d041fc700b95
Arne Govaerts <arne.govaerts@student.hogent.be>
2020-03-08 14:51:46 +0100

    mixed

---
+++ arnold-analytics.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: Arnold Analytics
+ * Version: 1.0.0
+ * Plugin URI: https://usearnold.com/
+ * Description: This is your starter template for your next WordPress plugin.
+ * Author: Arne Govaerts
+ * Author URI: https://usearnold.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: arnold-analytics
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Arne Govaerts
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Load plugin class files.
+require_once 'includes/class-arnold-analytics.php';
+require_once 'includes/class-arnold-analytics-settings.php';
+
+// Load plugin libraries.
+require_once 'includes/lib/class-arnold-analytics-admin-api.php';
+require_once 'includes/lib/class-arnold-analytics-post-type.php';
+require_once 'includes/lib/class-arnold-analytics-taxonomy.php';
+
+/**
+ * Returns the main instance of Arnold_Analytics to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object Arnold_Analytics
+ */
+function arnold_analytics() {
+	$instance = Arnold_Analytics::instance( __FILE__, '1.0.0' );
+
+	if ( is_null( $instance->settings ) ) {
+		$instance->settings = Arnold_Analytics_Settings::instance( $instance );
+	}
+
+	return $instance;
+}
+
+arnold_analytics();
